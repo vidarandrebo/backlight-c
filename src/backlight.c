c562c8ab5562c9525cff434f61d6b4851fc08795
vidarandrebo <va.bo@stud.uis.no>
2020-10-26 15:09:24 +0100

    this is working now

---
+++ src/backlight.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define LOWER 0
+#define UPPER 120000
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        return 0;
+    }
+    FILE *fp;
+    fp = fopen("/sys/class/backlight/intel_backlight/brightness" ,"r+");
+    char light[20];
+    fscanf (fp, "%s", light);
+    //Needed for writing at start of file
+    fseek(fp, 0, SEEK_SET);
+    int lightint = atoi(light);
+    int a = atoi(argv[2]);
+    char k = argv[1][0];
+    switch (k) {
+        case '+':
+            lightint += a; 
+            break;
+        case '-':
+            lightint -= a;
+            break;
+        default:
+            fclose(fp);
+            return 0;
+    }
+    if (lightint > UPPER) {
+        lightint = UPPER;
+    }
+    if (lightint < LOWER) {
+        lightint = LOWER;
+    }
+    fprintf(fp, "%d\n",lightint);
+    fclose(fp);
+    return 0;
+}
